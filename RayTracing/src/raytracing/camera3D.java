03f4e7092e5aa7d6ed330053b0ed138d72ca0652
John Mamish <john.mamish@gmail.com>
2012-11-22 03:11:44 -0600

    Created new branch to fix up camera on.

--- RayTracing/src/raytracing/camera3D.java
+++ RayTracing/src/raytracing/camera3D.java
@@ -1,4 +1,4 @@
-package raytracing;
+package RayTracing;
 
 /**
  * This object is used in the creation of 3D images by providing the ray objects to the scene graph
@@ -7,9 +7,25 @@ package raytracing;
 public class camera3D
 {
     private vector3D perspective, direction, directionX, directionY;//orientation of the camera
-    private int horizResolution, vertResolution;//resolution of the images produced
+    private double horizResolution, vertResolution;//resolution of the images produced
     private double horizViewport, vertViewport;//angles of the viewport of the camera
+    final double OBSERVER_DISTANCE = 5;
     
+    public camera3D()
+    {
+        //starting at origin.
+        perspective = new vector3D(10, 0, 0);
+        
+        //direction is always <1, 0, 0>
+        //also direction should always have length 1.
+        direction = new vector3D(-1, 0, 0);
+        
+        directionX = new vector3D(0, -1, 0);
+        directionY = new vector3D(0, 0, 1); 
+                
+        horizResolution = 500;
+        vertResolution = 500;
+    }
     /**
      * creates a new camera3D object using vectors as orientation arguments
      * @param p - the position of the camera
@@ -20,17 +36,18 @@ public class camera3D
      * @param hv - the horizontal viewport angle of the camera
      * @param vv - the vertical viewport angle of the camera
      */
-    public camera3D(vector3D p, vector3D d, vector3D dx, int hr, int vr, double hv, double vv)
-    {//assign the appropriate values
+    /*public camera3D(vector3D p, vector3D d, int hr, int vr)
+    {
+        //assign the appropriate values
         perspective = p;
         direction = d;
-        directionX = dx;
+        directionX = new vector3D(0, 0, 0);
         directionY = vector3D.getCrossProduct(direction, directionX);
         horizResolution = hr;
         vertResolution = vr;
-        horizViewport = hv;
-        vertViewport = vv;
-    }
+        //horizViewport = hv;
+        //vertViewport = vv;
+    }*/
     
     /**
      * creates a new camera3D object using angles as orientation arguments
@@ -43,28 +60,28 @@ public class camera3D
      * @param hv - the horizontal viewport angle of the camera
      * @param vv - the vertical viewport angle of the camera
      */
-    public camera3D(vector3D p, double tilt, double yaw, double roll, int hr, int vr, double hv, double vv)
+    /*public camera3D(vector3D p, double tilt, double yaw, double roll, int hr, int vr, double hv, double vv)
     {//assign the proper values
         perspective = p;
-        direction = vector3D.angleToVector(yaw, tilt);
+        direction = new vector3D(1, 0, 0);//vector3D.angleToVector(yaw, tilt);
         directionX = vector3D.angleToVector(yaw + (Math.PI/2), roll);
         directionY = vector3D.getCrossProduct(direction, directionX);
         horizResolution = hr;
         vertResolution = vr;
         horizViewport = hv;
         vertViewport = vv;
-    }
+    }*/
     
     //accessor methods
     
     public int getHorizRes()
     {
-        return horizResolution;
+        return (int)horizResolution;
     }
     
     public int getVertRes()
     {
-        return vertResolution;
+        return (int)vertResolution;
     }
     
     public vector3D getPosition()
@@ -97,14 +114,23 @@ public class camera3D
      * or null if the x or y values are out of the bounds of the camera's resolution
      */
     public line3D getRayForPixel(int x, int y)
-    {//check for out of bounds exceptions
+    {
+        //check for out of bounds exceptions
         if ((x < 0 || x >= horizResolution) || (y < 0 || y >= vertResolution))
             return null;
-        vector3D xComponent = vector3D.scaleVector(directionX, ((double)x - ((double)horizResolution/2))/(double)horizResolution);
-        vector3D yComponent = vector3D.scaleVector(directionY, ((double)y - ((double)vertResolution/2))/(double)vertResolution);
+        
+        //x and y with a range [-1, 1]
+        double xRespect = ((x-(horizResolution/2))/(horizResolution/2));
+        double yRespect = ((y-(vertResolution/2))/(vertResolution/2));
+        
         //combine the components and normalize
-        vector3D dir = vector3D.add(direction, vector3D.add(xComponent, yComponent));
-        return new line3D(perspective, dir);
+        //with the predetermined angle and perspective, we know that an pixel x-shift corresponds to a y-shift and a pixel y-shift corresponds to z-shift        
+        vector3D pixelPoint = vector3D.add(this.perspective, vector3D.add(vector3D.scaleVector(directionX, xRespect), vector3D.scaleVector(directionY, yRespect)));
+        vector3D dir = vector3D.add(this.getDirection(), pixelPoint);
+        
+        //System.out.println(dir);
+        
+        return new line3D(perspective, vector3D.normalize(dir));
     }
     
     //mutator methods
@@ -115,14 +141,45 @@ public class camera3D
      * @param yaw - if looking along the X axis, yaw is the angle between direction and the X axis in the XZ plane
      * @param roll - if looking along the X axis, roll is the angle between direction and the Z axis in the YZ plane
      */
-    public void pointInDirection(double tilt, double yaw, double roll)
+    public void rotateXY(double rads)
     {
-        double Tilt = tilt % (Math.PI * 2);//make the numbers clean
-        double Yaw = yaw % (Math.PI * 2);
-        double Roll = roll % (Math.PI * 2);
-        direction = vector3D.angleToVector(Yaw, Tilt);//magic stuff
-        directionX = vector3D.angleToVector(Roll, Yaw);
-        directionY = vector3D.getCrossProduct(direction, directionX);
+        //recalculate direction vector.
+        double lengthProj = Math.sqrt(Math.pow(this.direction.getX(), 2) + Math.pow(this.direction.getY(), 2));
+        System.out.println("lengthproj = " + lengthProj);
+        
+        double theta;
+        if(this.direction.getX() == 0)
+        {
+            theta = (Math.PI/2)*Math.signum(this.direction.getY());
+        }
+        else
+        {
+            theta = Math.atan(this.direction.getY()/this.direction.getX());
+        }
+        
+        theta += rads;
+        
+        this.direction = new vector3D(lengthProj*Math.cos(theta), lengthProj*Math.sin(theta), this.direction.getZ());
+        
+        System.out.println("direction = " + direction);
+        
+        //this part must change to accomidate anything more complex than what exists now.
+        //recalculate x direction vector in the same manner we did the direction vector.  It lies entirely in the xy plane FOR NOW, so no need to project.
+        lengthProj = Math.sqrt(Math.pow(this.directionX.getX(), 2) + Math.pow(this.directionX.getY(), 2));
+        if(this.directionX.getX() == 0)
+        {
+            theta = (Math.PI/2)*Math.signum(this.directionX.getX());
+        }
+        else
+        {
+            theta = Math.atan(this.directionX.getY()/this.directionX.getX());
+        }
+        
+        theta += rads;
+        
+        this.directionX = new vector3D(lengthProj*Math.cos(theta), lengthProj*Math.sin(theta), this.directionX.getZ());
+        
+        System.out.println("XDir = " + directionX);
     }
     
     public void moveForward()
@@ -135,6 +192,17 @@ public class camera3D
         perspective = vector3D.add(perspective, vector3D.scaleVector(direction, -1));
     }
     
+    public double calcTheta()
+    {
+        if(this.direction.getX() == 0)
+        {
+            return (Math.PI/2)*Math.signum(this.direction.getY());
+        }
+        else
+        {
+            return Math.atan(this.direction.getY()/this.direction.getX());
+        }
+    }
     public void moveUp()
     {
         perspective = vector3D.add(perspective, directionY);
